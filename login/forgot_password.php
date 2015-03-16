c52eaee95044735dd1889a58b3ff0a6df12b4bb0
Justin Chi <j.chi2241@gmail.com>
2015-03-15 19:44:04 -0700

    modify temp password generator

--- login/forgot_password.php
+++ login/forgot_password.php
@@ -31,7 +31,7 @@
 
 
 				function random_password( $length = 8 ) {
-				    $chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_-=+;:,.?";
+				    $chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!?@#$";
 				    $password = substr( str_shuffle( $chars ), 0, $length );
 				    return $password;
 				}
